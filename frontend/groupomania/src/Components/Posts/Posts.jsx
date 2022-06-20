329007f9d91bd61ece078afb7595802f2e88956e
StevenAub <steven-aubry@outlook.fr>
2022-06-20 09:37:20 +0200

    modifs

--- frontend/groupomania/src/Components/Posts/Posts.jsx
+++ frontend/groupomania/src/Components/Posts/Posts.jsx
@@ -12,7 +12,6 @@ import axios from "axios";
 import styled from "styled-components";
 import TextField from "@mui/material/TextField";
 import LikePost from "../Like/Likes";
-import { width } from "@mui/system";
 
 const token = JSON.parse(localStorage.getItem("tokens"));
 
@@ -119,9 +118,6 @@ function Post() {
     }
   }, [alert, newPost]);
 
-  /*Primaire : #FD2D01
-● Secondaire : #FFD7D7
-● Tertiaire : #4E5166*/
   return (
     <div
       style={{
@@ -187,7 +183,7 @@ function Post() {
               <Card
                 key={`${post.title}-${index}`}
                 sx={{
-                  width: 750,
+                  width: "100%",
                   maxWidth: 750,
                   minWidth: 200,
                   marginTop: 5
@@ -280,121 +276,3 @@ function Post() {
 }
 
 export default Post;
-
-/*
-
-function Search() {
-  const token = JSON.parse(localStorage.getItem("tokens"));
-
-  const [datas, setDatas] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/post", {
-      headers: { authorization: `Bearer ${token}` }
-    })
-      .then((response) => response.json())
-      .then((data) => setDatas(data));
-  }, [token]);
-
-  const PostData = datas?.posts;
-  const handleSearch = (e) => {
-    let value = e.target.value;
-    value.length > 2 && setSearch(value);
-  };
-
-  console.log(search);
-  return (
-    <>
-      <div>
-        <div className="search">
-          <input
-            type="text"
-            name="searchBar"
-            id="searchBar"
-            placeholder="Rechercher sur groupomania"
-            onChange={handleSearch}
-          />
-        </div>
-        <div className="search_results">
-          {PostData?.filter((val) => {
-            return val.title.toLowerCase().includes(search.toLowerCase());
-          }).map((post, index) => (
-            <Stack justifyContent="center" alignItems="center" spacing={2}>
-              <Card
-                key={`${post.title}-${index}`}
-                sx={{ minWidth: 600, maxWidth: 600, minHeight: 350 }}
-              >
-                <CardMedia
-                  component="img"
-                  height="300"
-                  image={post.imgUrl}
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {post.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {post.content}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Commenter</Button>
-                  <Button size="small">Partager</Button>
-                </CardActions>
-              </Card>
-            </Stack>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default function AllPost() {
-  const { data, error } = useFetch(`http://localhost:3000/api/post`);
-
-  const PostData = data?.posts;
-
-  if (error) {
-    return <span>Oups il y a eu un problème</span>;
-  }
-
-  return (
-    <div>
-      {" "}
-      <Search />
-      <div>
-        <Stack justifyContent="center" alignItems="center" spacing={2}>
-          {PostData?.map((post, index) => (
-            <Card
-              key={`${post.title}-${index}`}
-              sx={{ minWidth: 600, maxWidth: 600, minHeight: 350 }}
-            >
-              <CardMedia
-                component="img"
-                height="300"
-                image={post.imgUrl}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {post.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {post.content}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Commenter</Button>
-                <Button size="small">Partager</Button>
-              </CardActions>
-            </Card>
-          ))}
-        </Stack>
-      </div>
-    </div>
-  );
-}
-*/
